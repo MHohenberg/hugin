50cfd171319af995a63a28d4a64ff2261f8f5552
root <root@lvps83-169-17-93.dedicated.hosteurope.de>
2017-02-21 21:29:40 +0100

    0.1! API call works, minor changes in layout

--- js/hugin.js
+++ js/hugin.js
@@ -1,9 +1,17 @@
+var listDomains = [];
+$('#alert_malicious').hide();
+$('#warning_dns').hide();
+
+
 function populateTable() {
 
     $.getJSON('domainlist.json', function (data) {
+	listDomains = [];
 	for (var i = 0; i < data.domainlist.length; i++) {
 	    $('#domainTable tbody').append("<tr><td id='"+data.domainlist[i].domain+"_name'>"+data.domainlist[i].domain+"</td><td id='"+data.domainlist[i].domain+"_ip'>"+data.domainlist[i].ip+"</td><td id='"+data.domainlist[i].domain+"_safeWebresult'></td></tr>");
+            listDomains.push(data.domainlist[i].domain);
 	}
+	querySafeBrowsingAPI();
     });
 }
 
@@ -21,19 +29,27 @@ function querySafeBrowsingAPI() {
         obj.threatInfo.platformTypes = ["WINDOWS"];
 	obj.threatInfo.threatEntryTypes = ["URL"];
 	obj.threatInfo.threatEntries = [];
-	obj.threatInfo.threatEntries.push({"url":"jugendzimmergestalten.de"});
-
-alert(apikey);
-alert(JSON.stringify(obj));
+	
+	for (var i = 0; i < listDomains.length; i++) {
+		obj.threatInfo.threatEntries.push({"url":listDomains[i]});
+	}
 
 	
         $.ajax({
 	    type: "POST",
-	    url: "https://safebrowsing.googleapis.com/v4/threatMatches:find?key="+apikey+" HTTP/1.1",
+	    url: "https://safebrowsing.googleapis.com/v4/threatMatches:find?key="+apikey+"",
             data:JSON.stringify(obj),
             contentType: "application/json; charset=utf-8",
 	    dataType: "json",
-            success:function(data){ alert(JSON.stringify(data)); },
+            success:function(data){
+
+		$("[id*='_safeWebresult']").html('<span class="label label-success">OK</span>')
+
+                for (var i = 0; i < data.matches.length; i++) {
+			$('#alert_malicious').show();
+			$("td[id='"+data.matches[i].threat.url+"_safeWebresult']").html('<span class="label label-danger">Danger</span>'); 
+                }
+            },
             failure: function(errMsg) {alert(errMsg)}
 	});
 
